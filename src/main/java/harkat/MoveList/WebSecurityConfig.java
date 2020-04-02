a4755661a6c2cecfabaffc832043682b995a707b
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-04-02 13:33:34 +0300

    RESTful services and boostrap styling

--- src/main/java/harkat/MoveList/WebSecurityConfig.java
+++ src/main/java/harkat/MoveList/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        .authorizeRequests().antMatchers("/css/**", "/index", "/movelist").permitAll()
+        .authorizeRequests().antMatchers("/css/**", "/index", "/movelist", "/moves", "/**").permitAll()
         .and()
         .authorizeRequests()
         	.anyRequest()
--- src/main/java/harkat/MoveList/web/MoveController.java
+++ src/main/java/harkat/MoveList/web/MoveController.java
@@ -1,35 +1,73 @@
 package harkat.MoveList.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import harkat.MoveList.domain.Move;
 import harkat.MoveList.domain.MoveRepository;
+import harkat.MoveList.domain.MoveType;
 import harkat.MoveList.domain.MoveTypeRepository;
+import harkat.MoveList.domain.Position;
 import harkat.MoveList.domain.PositionRepository;
 
 @Controller
 public class MoveController {
 	@Autowired
-	private MoveRepository moverepository;
+	MoveRepository moverepository;
 	@Autowired
-	private MoveTypeRepository movetyperepository;
+	MoveTypeRepository movetyperepository;
 	@Autowired
-	private PositionRepository positionrepository;
-	
+	PositionRepository positionrepository;
+
 	@RequestMapping(value= "/login")
 	public String login() {
 		return "login";
 	}
-	
+
 	@RequestMapping(value = {"/index", "/movelist"}, method = RequestMethod.GET)
 	public String displayMoveList(Model model) {
 		model.addAttribute("moves", moverepository.findAll());
 		return "movelist";
 	}
+
+	// RESTfull service methods
+
+	// all moves
+	@RequestMapping(value="/moves", method  = RequestMethod.GET) 
+	public @ResponseBody List<Move> listMovesRest(){
+		return (List<Move>) moverepository.findAll();
+	}
 	
+	// single move by id
+	@RequestMapping(value="/moves/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Move> getMoveById(@PathVariable("id") Long moveId) {	
+		return moverepository.findById(moveId);
+	}
 	
+	// all positions
+	@RequestMapping(value="/positions", method  = RequestMethod.GET) 
+	public @ResponseBody List<Position> listPositionsRest(){
+		return (List<Position>) positionrepository.findAll();
+	}
+
+	// all types
+	@RequestMapping(value="/movetypes", method  = RequestMethod.GET) 
+	public @ResponseBody List<MoveType> listMoveTypesRest(){
+		return (List<MoveType>) movetyperepository.findAll();
+	}
+
+
+
+
+
 
 }
