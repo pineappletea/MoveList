deca571a91f13f3f5ba5bf95ee0a8a11faf54853
pineappletea <47184314+pineappletea@users.noreply.github.com>
2020-04-03 16:42:16 +0300

    tests

--- src/main/java/harkat/MoveList/domain/Move.java
+++ src/main/java/harkat/MoveList/domain/Move.java
@@ -8,8 +8,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
--- src/test/java/harkat/MoveList/MoveListApplicationTests.java
+++ src/test/java/harkat/MoveList/MoveListApplicationTests.java
@@ -1,13 +1,23 @@
 package harkat.MoveList;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import harkat.MoveList.web.MoveController;
+
 
 @SpringBootTest
 class MoveListApplicationTests {
 
+	@Autowired
+	private MoveController controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
